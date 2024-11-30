949b2e09210c97c983507fe9e785fa4048db23ff
Zalman Ul Fariz P A <103169802+mbedsyst@users.noreply.github.com>
2024-11-30 23:46:16 +0530

    Removed private function headers

--- Inc/GUARD.h
+++ Inc/GUARD.h
@@ -4,8 +4,5 @@
 #include "stm32f4xx.h"
 
 void StackGuard_Init(uint32_t guard_size);
-void StackGuard_Enable(void);
-void StackGuard_Disable(void);
-void StackGuard_SetHandler(void (*handler)(void));
 
 #endif
